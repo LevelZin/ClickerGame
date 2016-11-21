a0b5577369939cb99e53f33ef110bb5e9bb7c49b
Maria Karlsson <karlssonmaria1984@gmail.com>
2016-11-21 23:01:47 +0100

    Added basic scripts.

---
+++ ExJobb/Assets/Scripts/Click.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+    public UnityEngine.UI.Text mGoldDisplay;
+    
+    public float currentGold = 0.0f;
+    public float goldPerClick = 1.0f;
+
+    void Update()
+    {
+        mGoldDisplay.text = "Gold: " + currentGold;
+    }
+
+    public void Clicked()
+    {
+        currentGold += goldPerClick;
+    }
+
+}
